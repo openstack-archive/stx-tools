c7b28fecdb8600b7ab543d362dfce62af9ceb2cc
Abraham Arce <abraham.arce.moreno@intel.com>
2018-08-03 04:48:22 -0500

    [RFC][Tar Compressed] Repository Changes
    - Cheksum Added
    - ...
    
    ToDo
    
    - integrity-kmod-e6aef069.tar.gz
    - mvn.repo.tgz
    - tpm-kmod-e6aef069.tar.gz
    
    Story: 2002736
    Task: 24217
    
    Change-Id: I1f20de7b1f1777e1d11ca14ceb6c22a86e33e045
    Signed-off-by: Abraham Arce <abraham.arce.moreno@intel.com>

--- centos-mirror-tools/tarball-dl.sh
+++ centos-mirror-tools/tarball-dl.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+#set -x
+
 # The build of StarlingX relies, besides RPM Binaries and Sources, in this
 # repository which is a collection of packages in the form of Tar Compressed
 # files and 3 RPMs obtained from a Tar Compressed file. This script and a text
@@ -10,100 +12,116 @@
 # input files:
 # The files tarball-dl.lst and mvn-artifacts.lst contain the list of packages
 # and artifacts for building this sub-mirror.
+
 script_path="$(dirname $(readlink -f $0))"
-tarball_file="$script_path/tarball-dl.lst"
+tarball_mirror="$script_path/${1}"
+tarball_list="$script_path/tarball-dl.lst"
 mvn_artf_file="$script_path/mvn-artifacts.lst"
 
-if [ ! -e $tarball_file -o ! -e $mvn_artf_file ];then
-    echo "$download_list does not exist, please have a check!"
+if [ ! -e $tarball_list -o ! -e $mvn_artf_file ];then
+    echo "$tarball_list does not exist, please have a check!"
     exit -1
 fi
 
 # The 2 categories we can divide the list of packages in the output directory:
 # - General hosted under "downloads" output directory.
 # - Puppet hosted under "downloads/puppet" output directory.
-# to be populated under $MY_REPO/addons/wr-cgcs/layers/cgcs/downloads/puppet
+# to be soft linked under build container $MY_REPO/addons/wr-cgcs/layers/cgcs/
 
-logs_dir="$script_path/logs"
-output_main="$script_path/output"
-output_log="$logs_dir/log_download_tarball_missing.txt"
-output_path=$output_main/stx-r1/CentOS/pike
-output_tarball=$output_path/downloads
+output_tarball=$tarball_mirror
 output_puppet=$output_tarball/puppet
+
+# Log Directory
+logs_dir="$script_path/logs"
+output_log="$logs_dir/log_download_tarball_missing.txt"
+
+tarball_list_failed="tarball-dl.failed"
+[ -f $tarball_list_failed ] && rm $tarball_list_failed
 
 mkdir -p $output_tarball
 mkdir -p $output_puppet
+
 if [ ! -d "$logs_dir" ]; then
     mkdir "$logs_dir"
 fi
 
-# Download function using wget command
-
-download_package() {
-    wget --spider $1
+download_package_wget() {
+    wget --spider ${1}
     if [ $? != 0 ]; then
         echo "$1 is broken"
     else
-        wget -t 5 --wait=15 $1
+        wget -t 5 --wait=1 ${1} -O ${2}
         if [ $? != 0 ]; then
             echo "$1" > "$output_log"
         fi
     fi
 }
 
-# This script will iterate over the tarball.lst text file and execute specific
-# tasks based on the name of the package:
+check_md5_package() {
+    status=0
+    md5=`md5sum ${1} | awk '{ print $1 }'`
+    echo "Package: ${1}"
+    if [ ${md5} == ${2} ]; then
+        echo "Checksum Ok"
+    elif [ ${2} == "Skip" ]; then
+        echo "Checksum Skipped"
+    else
+        echo "Checksum Failed"
+        status=1
+    fi
+    return $status
+}
 
-for line in $(cat $tarball_file); do
+if [ ! -d "$tarball_mirror" ]; then
+     echo "Tarball repository does not exist, creating..."
+     mkdir -p $tarball_mirror
+fi
 
-    # A line from the text file starting with "#" character is ignored
+for line in $(cat $tarball_list); do
 
     if [[ "$line" =~ ^'#' ]]; then
         echo "Skip $line"
         continue
     fi
 
-    # The text file contains 3 columns separated by a character "#"
-    # - Column 1, name of package including extensions as it is referenced
-    #   by the build system recipe, character "!" at the beginning of the name package
-    #   denotes special handling is required tarball_name=`echo $line | cut -d"#" -f1-1`
-    # - Column 2, name of the directory path after it is decompressed as it is
-    #   referenced in the build system recipe.
-    # - Column 3, the URL for the package download
-
-    tarball_name=$(echo $line | cut -d"#" -f1-1)
-    directory_name=$(echo $line | cut -d"#" -f2-2)
-    tarball_url=$(echo $line | cut -d"#" -f3-3)
-
-    # - For the General category and the Puppet category:
-    #   - Packages have a common process: download, decompressed,
-    #     change the directory path and compressed.
+    tarball_name=$(echo $line | cut -d"," -f1-1)
+    directory_name=$(echo $line | cut -d"," -f2-2)
+    tarball_checksum=$(echo $line | cut -d"," -f3-3)
+    tarball_url=$(echo $line | cut -d"," -f4-4)
 
     if [[ "$line" =~ ^pupp* ]]; then
-        download_path=$output_puppet/$tarball_name
-        download_directory=$output_puppet
+        tarball_directory=$output_puppet
+        tarball_path=$output_puppet/$tarball_name
     else
-        download_path=$output_tarball/$tarball_name
-        download_directory=$output_tarball
+        tarball_directory=$output_tarball
+        tarball_path=$output_tarball/$tarball_name
     fi
 
-    # We have 6 packages from the text file starting with the character "!":
-    # they require special handling besides the common process: remove directory,
-    # remove text from some files, clone a git repository, etc.
+    if [[ "$line" =~ ^MLNX_OFED_LINUX ]]; then
+        tarball_path=$output_tarball/$directory_name
+    fi
 
-    if [[ "$line" =~ ^'!' ]]; then
-        tarball_name="${tarball_name//!/}"
-        echo $tarball_name
-        pushd $output_tarball
-        if [ "$tarball_name" = "integrity-kmod-e6aef069.tar.gz" ]; then
-            download_package $tarball_url
+    pushd $tarball_directory
+
+    if [ ! -e $tarball_path ]; then
+        download_package_wget $tarball_url $tarball_path
+        if [[ "$tarball_name" =~ ^integrity-kmod* ]]; then
             tar xf e6aef069b6e97790cb127d5eeb86ae9ff0b7b0e3.tar.gz
             mv linux-tpmdd-e6aef06/security/integrity/ $directory_name
             tar czvf $tarball_name $directory_name
             rm -rf linux-tpmdd-e6aef06
+            rm -rf $directory_name
             rm e6aef069b6e97790cb127d5eeb86ae9ff0b7b0e3.tar.gz
-        elif [ "$tarball_name" = "mariadb-10.1.28.tar.gz" ]; then
-            download_package $tarball_url
+        elif [[ "$tarball_name" =~ ^tpm-kmod* ]]; then
+            tar xf e6aef069b6e97790cb127d5eeb86ae9ff0b7b0e3.tar.gz
+            mv linux-tpmdd-e6aef06/drivers/char/tpm $directory_name
+            tar czvf $tarball_name $directory_name
+            rm -rf linux-tpmdd-e6aef06
+            rm -rf $directory_name
+            rm e6aef069b6e97790cb127d5eeb86ae9ff0b7b0e3.tar.gz
+        elif [[ "$tarball_name" =~ ^qat1.7.upstream* ]]; then
+            echo "None"
+        elif [[ "$tarball_name" =~ ^mariadb* ]]; then
             mkdir $directory_name
             tar xf $tarball_name --strip-components 1 -C $directory_name
             rm $tarball_name
@@ -115,31 +133,25 @@ for line in $(cat $tarball_file); do
             popd
             tar czvf $tarball_name $directory_name
             rm -rf $directory_name
-        # The mvn.repo.tgz tarball will be created downloading a serie of
-        # of maven artifacts described in mvn-artifacts file.
-        elif [ "$tarball_name" = "mvn.repo.tgz" ]; then
-            mkdir -p "$directory_name"
-            if [ ! -f "$mvn_artf_file" ]; then
-                echo "$mvn_artf_file no found" 1>&2
-                exit 1
-            fi
-            while read -r artf; do
-                echo "download: $(basename $artf)"
-                wget "$tarball_url/$artf" -P "$directory_name/$(dirname $artf)"
-            done < "$mvn_artf_file"
-
-            # Create tarball
-            tar -zcvf "$tarball_name" -C "$directory_name"/ .
-            rm  -rf "$directory_name"
-            mv "$tarball_name" "$download_directory"
-        elif [[ "$tarball_name" =~ ^'MLNX_OFED_LINUX' ]]; then
+        elif [[ "$tarball_name" =~ ^tss2-930* ]]; then
+            git clone https://git.code.sf.net/p/ibmtpm20tss/tss ibmtpm20tss-tss
+            pushd ibmtpm20tss-tss
+            git checkout v930
+            rm -rf .git
+            popd
+            mv ibmtpm20tss-tss $directory_name
+            tar czvf $tarball_name $directory_name
+            rm -rf $directory_name
+        elif [[ "$tarball_name" =~ ^MLNX_OFED_LINUX* ]]; then
+            mv $tarball_path $tarball_name
             pkg_version=$(echo "$tarball_name" | cut -d "-" -f2-3)
             srpm_path="MLNX_OFED_SRC-${pkg_version}/SRPMS/"
-            download_package "$tarball_url"
             tar -xf "$tarball_name"
+            tarball=$directory_name
+            directory_name=`echo $tarball_name | sed 's/.tgz//'`
             tar -xf "$directory_name/src/MLNX_OFED_SRC-${pkg_version}.tgz"
             # This section of code gets specific SRPMs versions according
-            # to the OFED tarbal version,
+            # to the OFED tarball version
             if [ "$pkg_version" = "4.2-1.2.0.0" ]; then
                 cp "$srpm_path/libibverbs-41mlnx1-OFED.4.2.1.0.6.42120.src.rpm" .
             elif [ "$pkg_version" = "4.3-1.0.1.0" ]; then
@@ -154,36 +166,7 @@ for line in $(cat $tarball_file); do
             rm -f "$tarball_name"
             rm -rf "MLNX_OFED_SRC-${pkg_version}"
             rm -rf "$directory_name"
-        elif [ "$tarball_name" = "qat1.7.upstream.l.1.0.3-42.tar.gz" ]; then
-            download_package $tarball_url
-        elif [ "$tarball_name" = "tpm-kmod-e6aef069.tar.gz" ]; then
-            download_package $tarball_url
-            tar xf e6aef069b6e97790cb127d5eeb86ae9ff0b7b0e3.tar.gz
-            mv linux-tpmdd-e6aef06/drivers/char/tpm $directory_name
-            tar czvf $tarball_name $directory_name
-            rm -rf linux-tpmdd-e6aef06
-            rm -rf $directory_name
-            rm e6aef069b6e97790cb127d5eeb86ae9ff0b7b0e3.tar.gz
-        elif [ "$tarball_name" = "tss2-930.tar.gz" ]; then
-            git clone https://git.code.sf.net/p/ibmtpm20tss/tss ibmtpm20tss-tss
-            pushd ibmtpm20tss-tss
-            git checkout v930
-            rm -rf .git
-            popd
-            mv ibmtpm20tss-tss $directory_name
-            tar czvf $tarball_name $directory_name
-            rm -rf $directory_name
-        fi
-        popd
-        continue
-    fi
-
-    download_cmd="wget -t 5 --wait=15 $tarball_url -O $download_path"
-
-    if [ ! -e $download_path ]; then
-        if $download_cmd ; then
-            echo "Ok: $download_path"
-            pushd $download_directory
+        else
             directory_name_original=$(tar -tf $tarball_name | head -1 | cut -f1 -d"/")
             if [ "$directory_name" != "$directory_name_original" ]; then
                 mkdir -p $directory_name
@@ -191,17 +174,14 @@ for line in $(cat $tarball_file); do
                 tar -czf $tarball_name $directory_name
                 rm -r $directory_name
             fi
-            popd
-        else
-            echo "Error: Failed to download $tarball_url" 1>&2
-            echo "$tarball_url" > "$output_log"
         fi
+    fi
 
-    else
-        echo "Already have $download_path"
+    popd
+
+    check_md5_package $tarball_path $tarball_checksum
+    if [[ $? != 0 ]]; then
+        echo $line >> $tarball_list_failed
     fi
 
 done
-
-# End of file
-
