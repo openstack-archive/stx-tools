4bf531930bc1208b0d1de26a343653318048a215
slin14 <shuicheng.lin@intel.com>
2018-09-26 20:26:28 +0800

    upgrade mlnx network driver to 4.4-2.0.7.0 version
    Story: 2003597
    Task: 26589
    
    3 src rpm are upgraded together: mlnx-ofa_kernel, rdma-core, libibverbs
    
    Change-Id: Ice11cea6db3748af312743ae5fb57396c6470981
    Signed-off-by: slin14 <shuicheng.lin@intel.com>

--- centos-mirror-tools/dl_tarball.sh
+++ centos-mirror-tools/dl_tarball.sh
@@ -145,21 +145,9 @@ for line in $(cat $tarball_file); do
             download_package "$tarball_url"
             tar -xf "$tarball_name"
             tar -xf "$directory_name/src/MLNX_OFED_SRC-${pkg_version}.tgz"
-            # This section of code gets specific SRPMs versions according
-            # to the OFED tarbal version,
-            if [ "$pkg_version" = "4.2-1.2.0.0" ]; then
-                cp "$srpm_path/libibverbs-41mlnx1-OFED.4.2.1.0.6.42120.src.rpm" .
-            elif [ "$pkg_version" = "4.3-1.0.1.0" ]; then
-                cp "$srpm_path/mlnx-ofa_kernel-4.3-OFED.4.3.1.0.1.1.g8509e41.src.rpm" .
-                cp "$srpm_path/rdma-core-43mlnx1-1.43101.src.rpm" .
-                cp "$srpm_path/libibverbs-41mlnx1-OFED.4.3.0.1.8.43101.src.rpm" .
-            elif [ "$pkg_version" = "4.3-3.0.2.1" ]; then
-                cp "$srpm_path/mlnx-ofa_kernel-4.3-OFED.4.3.3.0.2.1.gcf60532.src.rpm" .
-                cp "$srpm_path/rdma-core-43mlnx1-1.43302.src.rpm" .
-                cp "$srpm_path/libibverbs-41mlnx1-OFED.4.3.2.1.6.43302.src.rpm" .
-            else
-                echo "$pkg_version : unknown version"
-            fi
+            cp "$srpm_path/mlnx-ofa_kernel-4.4-OFED.4.4.2.0.7.1.gee7aa0e.src.rpm" .
+            cp "$srpm_path/rdma-core-43mlnx1-1.44207.src.rpm" .
+            cp "$srpm_path/libibverbs-41mlnx1-OFED.4.4.2.0.1.44207.src.rpm" .
             # Don't delete the original MLNX_OFED_LINUX tarball.
             # We don't use it, but it will prevent re-downloading this file.
             #   rm -f "$tarball_name"
