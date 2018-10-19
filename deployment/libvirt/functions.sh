8a94bdf4c36c293484fe1d07f820a15d9a83e7b8
Abraham Arce <abraham.arce.moreno@intel.com>
2018-10-19 09:48:10 -0500

    [Doc] XML Server Definition
    StarlingX deployment options are:
    
    - Standard Controller
      - Dedicated Storage
      - Controller Storage
    - All-in-one
      - Duplex
      - Simplex
    
    Installation guides should list the XML server definitions for each configuration
    and Storage server definition is missing in Dedicated Storage:
    
    - For each type of node, add create and destroy functions
    - Add storage.xml
    - Split the creation and removal of dedicated and controller storage in 2
      separate scripts to align with the installation guide naming convention.
    - Split the creation and removal of simplex and duplex in 2 separate
      scripts to align with the installation guide naming convention.
    - Update the README file to reflect a third configuration script.
    
    Change-Id: I50e8d83bc96e9f394293db29c1e59d1757023927
    Signed-off-by: Abraham Arce <abraham.arce.moreno@intel.com>

--- deployment/libvirt/functions.sh
+++ deployment/libvirt/functions.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
-# delete a node's disk file in a safe way
+DOMAIN_DIRECTORY=vms
+
+usage() {
+    echo "$0 [-h] [-i <iso image>]"
+    echo ""
+    echo "Options:"
+    echo "  -i: StarlingX ISO image"
+    echo ""
+}
+
+iso_image_check() {
+    local ISOIMAGE=$1
+    FILETYPE=$(file --mime-type -b ${ISOIMAGE})
+    if ([ "$FILETYPE" != "application/x-iso9660-image" ]); then
+        echo "$ISOIMAGE is not an application/x-iso9660-image type"
+        exit -1
+    fi
+}
+
+# Delete a node's disk file in a safe way
 delete_disk() {
     local fpath="$1"
 
@@ -18,7 +37,7 @@ delete_disk() {
     sudo rm "$fpath"
 }
 
-# delete an xml file in a safe way
+# Delete an xml file in a safe way
 delete_xml() {
     local fpath="$1"
 
@@ -36,3 +55,155 @@ delete_xml() {
     sudo rm "$fpath"
 }
 
+# Create a Controller node
+create_controller() {
+    local CONFIGURATION=$1
+    local CONTROLLER=$2
+    local BRIDGE_INTERFACE=$3
+    local ISOIMAGE=$4
+    local DOMAIN_FILE=${DOMAIN_DIRECTORY}/${CONTROLLER}.xml
+    if ([ "$CONFIGURATION" == "simplex" ]); then
+        CONTROLLER_NODE_NUMBER=0
+    else
+        CONTROLLER_NODE_NUMBER=1
+    fi
+    for ((i=0; i<=$CONTROLLER_NODE_NUMBER; i++)); do
+        CONTROLLER_NODE=${CONFIGURATION}-${CONTROLLER}-${i}
+        DOMAIN_FILE=${DOMAIN_DIRECTORY}/${CONTROLLER_NODE}.xml
+        if ([ "$CONFIGURATION" == "simplex" ] || [ "$CONFIGURATION" == "duplex" ]); then
+            DISK_0_SIZE=600
+            cp controller_allinone.xml ${DOMAIN_FILE}
+        else
+            DISK_0_SIZE=200
+            cp controller.xml ${DOMAIN_FILE}
+        fi
+        sed -i -e "
+            s,NAME,${CONTROLLER_NODE},
+            s,DISK0,/var/lib/libvirt/images/${CONTROLLER_NODE}-0.img,
+            s,DISK1,/var/lib/libvirt/images/${CONTROLLER_NODE}-1.img,
+            s,%BR1%,${BRIDGE_INTERFACE}1,
+            s,%BR2%,${BRIDGE_INTERFACE}2,
+            s,%BR3%,${BRIDGE_INTERFACE}3,
+            s,%BR4%,${BRIDGE_INTERFACE}4,
+        " ${DOMAIN_FILE}
+        sudo qemu-img create -f qcow2 /var/lib/libvirt/images/${CONTROLLER_NODE}-0.img ${DISK_0_SIZE}G
+        sudo qemu-img create -f qcow2 /var/lib/libvirt/images/${CONTROLLER_NODE}-1.img 200G
+        if ([ "$CONFIGURATION" == "simplex" ] || [ "$CONFIGURATION" == "duplex" ]); then
+            sed -i -e "
+                s,DISK2,/var/lib/libvirt/images/${CONTROLLER_NODE}-2.img,
+            " ${DOMAIN_FILE}
+            sudo qemu-img create -f qcow2 /var/lib/libvirt/images/${CONTROLLER_NODE}-2.img 200G
+        fi
+        if [ $i -eq 0 ]; then
+            sed -i -e "s,ISO,${ISOIMAGE}," ${DOMAIN_FILE}
+        else
+            sed -i -e "s,ISO,," ${DOMAIN_FILE}
+        fi
+        sudo virsh define ${DOMAIN_DIRECTORY}/${CONTROLLER_NODE}.xml
+        if [ $i -eq 0 ]; then
+            sudo virsh start ${CONTROLLER_NODE}
+        fi
+        echo "                                               " $i
+    done
+}
+
+# Delete a Controller node
+destroy_controller() {
+    local CONFIGURATION=$1
+    local CONTROLLER=$2
+    if ([ "$CONFIGURATION" == "simplex" ]); then
+        CONTROLLER_NODE_NUMBER=0
+    else
+        CONTROLLER_NODE_NUMBER=1
+    fi
+    for ((i=0; i<=$CONTROLLER_NODE_NUMBER; i++)); do
+        CONTROLLER_NODE=${CONFIGURATION}-${CONTROLLER}-${i}
+        DOMAIN_FILE=$DOMAIN_DIRECTORY/$CONTROLLER_NODE.xml
+        if virsh list --all --name | grep ${CONTROLLER_NODE}; then
+            STATUS=$(virsh list --all | grep ${CONTROLLER_NODE} | awk '{ print $3}')
+            if ([ "$STATUS" == "running" ])
+            then
+                sudo virsh destroy ${CONTROLLER_NODE}
+            fi
+            sudo virsh undefine ${CONTROLLER_NODE}
+            delete_disk /var/lib/libvirt/images/${CONTROLLER_NODE}-0.img
+            delete_disk /var/lib/libvirt/images/${CONTROLLER_NODE}-1.img
+            if ([ "$CONFIGURATION" == "simplex" ] || [ "$CONFIGURATION" == "duplex" ]); then
+                delete_disk /var/lib/libvirt/images/${CONTROLLER_NODE}-2.img
+            fi
+            [ -e ${DOMAIN_FILE} ] && delete_xml ${DOMAIN_FILE}
+        fi
+    done
+}
+
+# Create a Compute node
+create_compute() {
+    COMPUTE_NODE=$1
+    sudo qemu-img create -f qcow2 /var/lib/libvirt/images/${COMPUTE_NODE}-0.img 200G
+    sudo qemu-img create -f qcow2 /var/lib/libvirt/images/${COMPUTE_NODE}-1.img 200G
+    DOMAIN_FILE=${DOMAIN_DIRECTORY}/${COMPUTE_NODE}.xml
+    cp compute.xml ${DOMAIN_FILE}
+    sed -i -e "
+        s,NAME,${COMPUTE_NODE},;
+        s,DISK0,/var/lib/libvirt/images/${COMPUTE_NODE}-0.img,;
+        s,DISK1,/var/lib/libvirt/images/${COMPUTE_NODE}-1.img,
+        s,%BR1%,${BRIDGE_INTERFACE}1,
+        s,%BR2%,${BRIDGE_INTERFACE}2,
+        s,%BR3%,${BRIDGE_INTERFACE}3,
+        s,%BR4%,${BRIDGE_INTERFACE}4,
+    " ${DOMAIN_FILE}
+    sudo virsh define ${DOMAIN_FILE}
+}
+
+# Delete a Compute node
+destroy_compute() {
+    local COMPUTE_NODE=$1
+    local DOMAIN_FILE=$DOMAIN_DIRECTORY/$COMPUTE_NODE.xml
+    if virsh list --all --name | grep ${COMPUTE_NODE}; then
+        STATUS=$(virsh list --all | grep ${COMPUTE_NODE} | awk '{ print $3}')
+        if ([ "$STATUS" == "running" ])
+        then
+            sudo virsh destroy ${COMPUTE_NODE}
+        fi
+        sudo virsh undefine ${COMPUTE_NODE}
+        delete_disk /var/lib/libvirt/images/${COMPUTE_NODE}-0.img
+        delete_disk /var/lib/libvirt/images/${COMPUTE_NODE}-1.img
+        [ -e ${DOMAIN_FILE} ] && delete_xml ${DOMAIN_FILE}
+    fi
+}
+
+# Create a Storage node
+create_compute() {
+    local STORAGE_NODE=$1
+    local DOMAIN_FILE=${DOMAIN_DIRECTORY}/${STORAGE_NODE}.xml
+    sudo qemu-img create -f qcow2 /var/lib/libvirt/images/${STORAGE_NODE}-0.img 200G
+    sudo qemu-img create -f qcow2 /var/lib/libvirt/images/${STORAGE_NODE}-1.img 200G
+    cp storage.xml ${DOMAIN_FILE}
+    sed -i -e "
+        s,NAME,${STORAGE_NODE},;
+        s,DISK0,/var/lib/libvirt/images/${STORAGE_NODE}-0.img,;
+        s,DISK1,/var/lib/libvirt/images/${STORAGE_NODE}-1.img,
+        s,%BR1%,${BRIDGE_INTERFACE}1,
+        s,%BR2%,${BRIDGE_INTERFACE}2,
+        s,%BR3%,${BRIDGE_INTERFACE}3,
+        s,%BR4%,${BRIDGE_INTERFACE}4,
+    " ${DOMAIN_FILE}
+    sudo virsh define ${DOMAIN_FILE}
+}
+
+# Delete a Storage node
+destroy_storage() {
+    local STORAGE_NODE=$1
+    local DOMAIN_FILE=$DOMAIN_DIRECTORY/$STORAGE_NODE.xml
+    if virsh list --all --name | grep ${STORAGE_NODE}; then
+        STATUS=$(virsh list --all | grep ${STORAGE_NODE} | awk '{ print $3}')
+        if ([ "$STATUS" == "running" ])
+        then
+            sudo virsh destroy ${STORAGE_NODE}
+        fi
+        sudo virsh undefine ${STORAGE_NODE}
+        delete_disk /var/lib/libvirt/images/${STORAGE_NODE}-0.img
+        delete_disk /var/lib/libvirt/images/${STORAGE_NODE}-1.img
+        [ -e ${DOMAIN_FILE} ] && delete_xml ${DOMAIN_FILE}
+    fi
+}
