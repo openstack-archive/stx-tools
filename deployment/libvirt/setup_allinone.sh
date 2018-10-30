4e7a85b2eb40432c83e156ad8ae2f1a6420e9068
Abraham Arce <abraham.arce.moreno@intel.com>
2018-10-30 09:14:40 -0600

    [Doc] Controller Identity Nodes Common Code
    The creation of the Controller identity nodes is common between
    all-in-one and standard controller configuration setups.
    
    Make both scripts shared same code located at a common functions
    file.
    
    Change-Id: I2ff7d43bd01a2a40264c442d8031a83e1dbbfadb
    Signed-off-by: Abraham Arce <abraham.arce.moreno@intel.com>

--- deployment/libvirt/setup_allinone.sh
+++ deployment/libvirt/setup_allinone.sh
@@ -23,42 +23,15 @@ fi
 
 iso_image_check ${ISOIMAGE}
 
+CONFIGURATION="allinone"
 BRIDGE_INTERFACE=${BRIDGE_INTERFACE:-stxbr}
-CONTROLLER=${CONTROLLER:-controller-allinone}
+CONTROLLER=${CONTROLLER:-controller}
 DOMAIN_DIRECTORY=vms
-DOMAIN_FILE=$DOMAIN_DIRECTORY/$CONTROLLER.xml
 
 bash ${SCRIPT_DIR}/destroy_allinone.sh
 
 [ ! -d ${DOMAIN_DIRECTORY} ] && mkdir ${DOMAIN_DIRECTORY}
 
-for i in {0..1}; do
-    CONTROLLER_NODE=${CONTROLLER}-${i}
-    sudo qemu-img create -f qcow2 /var/lib/libvirt/images/${CONTROLLER_NODE}-0.img 600G
-    sudo qemu-img create -f qcow2 /var/lib/libvirt/images/${CONTROLLER_NODE}-1.img 200G
-    sudo qemu-img create -f qcow2 /var/lib/libvirt/images/${CONTROLLER_NODE}-2.img 200G
-    ISOIMAGE=${ISOIMAGE}
-    DOMAIN_FILE=${DOMAIN_DIRECTORY}/${CONTROLLER_NODE}.xml
-    cp ${SCRIPT_DIR}/controller_allinone.xml ${DOMAIN_FILE}
-    sed -i -e "
-        s,NAME,${CONTROLLER_NODE},
-        s,DISK0,/var/lib/libvirt/images/${CONTROLLER_NODE}-0.img,
-        s,DISK1,/var/lib/libvirt/images/${CONTROLLER_NODE}-1.img,
-        s,DISK2,/var/lib/libvirt/images/${CONTROLLER_NODE}-2.img,
-        s,%BR1%,${BRIDGE_INTERFACE}1,
-        s,%BR2%,${BRIDGE_INTERFACE}2,
-        s,%BR3%,${BRIDGE_INTERFACE}3,
-        s,%BR4%,${BRIDGE_INTERFACE}4,
-    " ${DOMAIN_FILE}
-    if [ $i -eq 0 ]; then
-        sed -i -e "s,ISO,${ISOIMAGE}," ${DOMAIN_FILE}
-    else
-        sed -i -e "s,ISO,," ${DOMAIN_FILE}
-    fi
-    sudo virsh define ${DOMAIN_DIRECTORY}/${CONTROLLER_NODE}.xml
-    if [ $i -eq 0 ]; then
-        sudo virsh start ${CONTROLLER_NODE}
-    fi
-done
+create_controller $CONFIGURATION $CONTROLLER $BRIDGE_INTERFACE $ISOIMAGE
 
 sudo virt-manager
