e6ae449efbd9a89e7fb3c9e23736c876e4a16ad1
Abraham Arce <abraham.arce.moreno@intel.com>
2018-10-31 03:36:40 -0600

    [Doc] Setup Configurations Clean Up
    Combine setup_allinone.sh and setup_standard_controller.sh to have one
    single setup script to configure via input parameter allinone and
    standardcontroller configurations:
    
      - setup_configuration.sh
      - destroy_configuration.sh
    
    Change-Id: Id29d3eeeff43f0c0f43ab710a1179eaacdfb330d
    Signed-off-by: Abraham Arce <abraham.arce.moreno@intel.com>

---
+++ deployment/libvirt/destroy_configuration.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}" )" )"
+
+source ${SCRIPT_DIR}/functions.sh
+
+while getopts "c:" o; do
+    case "${o}" in
+        c)
+            CONFIGURATION=${OPTARG}
+            ;;
+        *)
+            usage_destroy
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [[ -z ${CONFIGURATION} ]]; then
+    usage_destroy
+    exit -1
+fi
+
+CONFIGURATION=${CONFIGURATION:-allinone}
+CONTROLLER=${CONTROLLER:-controller}
+DOMAIN_DIRECTORY=vms
+
+destroy_controller ${CONFIGURATION} ${CONTROLLER}
+
+if ([ "$CONFIGURATION" == "standardcontroller" ]); then
+    COMPUTE=${COMPUTE:-compute}
+    COMPUTE_NODES_NUMBER=${COMPUTE_NODES_NUMBER:-1}
+    for ((i=0; i<=$COMPUTE_NODES_NUMBER; i++)); do
+        COMPUTE_NODE=${COMPUTE}-${i}
+        destroy_compute $COMPUTE_NODE
+    done
+fi
